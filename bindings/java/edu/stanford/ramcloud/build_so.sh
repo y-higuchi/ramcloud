c30cf9b177fe352f53f1c0d9cf1d8aed084cf9ca
Yuta HIGUCHI <y-higuchi@onlab.us>
2013-11-21 15:59:22 -0800

    Fix JNI shared lib creating script

--- bindings/java/edu/stanford/ramcloud/build_so.sh
+++ bindings/java/edu/stanford/ramcloud/build_so.sh
@@ -1,2 +1,5 @@
-c++ -g -Wall -O3 -shared -fPIC -std=c++0x -I/usr/lib/jvm/default-java/include/ -I${HOME}/ramcloud/src/ -I${HOME}/ramcloud/obj.master/ -I${HOME}/ramcloud/logcabin/ -I${HOME}/ramcloud/gtest/include/ -L${HOME}/ramcloud/obj.master -o libedu_stanford_ramcloud_JRamCloud.so edu_stanford_ramcloud_JRamCloud.cc -lramcloud
-
+JAVA_HOME=/usr/lib/jvm/java-7-oracle
+RAMCLOUD_HOME=~/ramcloud
+RAMCLOUD_BRANCH=blueprint-java
+c++ -g -Wall -O3 -shared -fPIC -std=c++0x -I${JAVA_HOME}/include/ -I${JAVA_HOME}/include/linux -I${RAMCLOUD_HOME}/src/ -I${RAMCLOUD_HOME}/obj.${RAMCLOUD_BRANCH}/ -I${RAMCLOUD_HOME}/logcabin/ -I${RAMCLOUD_HOME}/gtest/include/ -L${RAMCLOUD_HOME}/obj.${RAMCLOUD_BRANCH} -o libedu_stanford_ramcloud_JRamCloud.so edu_stanford_ramcloud_JRamCloud.cc -lramcloud
+ln -s `pwd`/libedu_stanford_ramcloud_JRamCloud.so ${RAMCLOUD_HOME}/obj.${RAMCLOUD_BRANCH}

4ad538e9c5cd618940c3d580b9ad00ea9fd56316
Yuta HIGUCHI <y-higuchi@onlab.us>
2013-11-21 16:30:30 -0800

    Add header generation code to script

--- bindings/java/edu/stanford/ramcloud/build_so.sh
+++ bindings/java/edu/stanford/ramcloud/build_so.sh
@@ -1,5 +1,6 @@
 JAVA_HOME=/usr/lib/jvm/java-7-oracle
 RAMCLOUD_HOME=~/ramcloud
 RAMCLOUD_BRANCH=blueprint-java
+javah -cp ../../.. edu.stanford.ramcloud.JRamCloud
 c++ -g -Wall -O3 -shared -fPIC -std=c++0x -I${JAVA_HOME}/include/ -I${JAVA_HOME}/include/linux -I${RAMCLOUD_HOME}/src/ -I${RAMCLOUD_HOME}/obj.${RAMCLOUD_BRANCH}/ -I${RAMCLOUD_HOME}/logcabin/ -I${RAMCLOUD_HOME}/gtest/include/ -L${RAMCLOUD_HOME}/obj.${RAMCLOUD_BRANCH} -o libedu_stanford_ramcloud_JRamCloud.so edu_stanford_ramcloud_JRamCloud.cc -lramcloud
 ln -s `pwd`/libedu_stanford_ramcloud_JRamCloud.so ${RAMCLOUD_HOME}/obj.${RAMCLOUD_BRANCH}
